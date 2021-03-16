4d9227aa57e4007016cbe367838ee7b3c2d305e2
Jürgen Knauth <pubsrc@binary-overflow.de>
2021-03-16 14:19:32 +0100

    Initial commit.

---
+++ testing/pack.py
@@ -0,0 +1,86 @@
+#!/usr/bin/python3
+
+
+
+
+import time
+import os
+import typing
+
+import jk_utils
+import jk_pathpatternmatcher2
+import jk_terminal_essentials
+import jk_uploadpack
+
+
+
+
+
+
+SRC_DIR_PATH = "/home/woodoo/DevPriv/Www/www.binary-overflow.de/output/wwwroot"
+OUTPUT_BASE_FILE_NAME = "pack.up"
+
+
+
+
+
+
+
+
+def compress(compression:str):
+	outFilePath = OUTPUT_BASE_FILE_NAME + (("." + compression) if compression else "")
+
+	t0 = time.time()
+
+	up = jk_uploadpack.Packer(outFilePath, compression)
+	up.fileGroup("default").bCleanDir = True
+
+	allFiles = []
+	for e in jk_pathpatternmatcher2.walk(
+			SRC_DIR_PATH,
+			acceptDirPathPatterns = None,
+			acceptFilePathPatterns = "**/*",
+			acceptLinkPathPatterns = None,
+			ignorePathPatterns = "**/__*",
+			ignoreDirPathPatterns = None,
+			ignoreFilePathPatterns = None,
+			ignoreLinkPathPatterns = None,
+			emitDirs = False,
+			emitFiles = True,
+			emitLinks = False,
+			emitBaseDirs = False,
+			recursive = True,
+			sort = True,
+			emitErrorEntries = True,
+			clazz = None,
+			ioAdapter = None,
+		):
+
+		allFiles.append((e.fullPath, e.relFilePath))
+
+	sp = jk_terminal_essentials.Spinner(len(allFiles))
+	for fullPath, relFilePath in allFiles:
+		sp.spin("packing", relFilePath)
+		up.fileGroup("default").addFile(fullPath, relFilePath)
+
+	up.close()
+	sp.hide()
+
+	print()
+	print("upload pack", compression if compression else "uncompressed")
+	print()
+	print("\ttotalSizeLogical =", jk_utils.formatBytes(up.totalSizeLogical))
+	print("\ttotalSizeUncompressed =", jk_utils.formatBytes(up.totalSizeUncompressed))
+	print("\ttotalSizeCompressed =", jk_utils.formatBytes(up.totalSizeCompressed))
+	print("\tduration:", jk_utils.formatTime(time.time() - t0, withMilliseconds=True))
+	print()
+
+#
+
+
+
+compress(None)
+compress("gz")
+compress("bz2")
+compress("xz")
+
---
+++ testing/unpack.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+
+
+
+import time
+import os
+import typing
+
+import jk_utils
+import jk_terminal_essentials
+import jk_uploadpack
+
+
+
+
+
+
+INPUT_FILE_NAME = "pack.up"
+
+
+
+up = jk_uploadpack.Unpacker(INPUT_FILE_NAME)
+
+sp = jk_terminal_essentials.Spinner(len(up.fileGroup("default").files))
+up.fileGroup("default").unpackToDir("out", sp)
+sp.hide()
+
+print()
+print("totalSizePacked =", jk_utils.formatBytes(up.totalSizePacked))
+print("totalSizeUnpacked =", jk_utils.formatBytes(up.totalSizeUnpacked))
+print()
+
+
